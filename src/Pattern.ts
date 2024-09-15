a69b33ad1e8597cdd4bc3819d7d3f80fedacf2f8
Kamioda <kamiodamasato@outlook.com>
2024-09-15 22:38:19 +0900

    システムID、仮想ID、アプリケーションIDのパターンを追加( #1 )

---
+++ src/Pattern.ts
@@ -0,0 +1,3 @@
+export const SystemIDPattern = /^\d{13}$/;
+export const VirtualIDPattern = /^vid-[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}$/;
+export const AppIDPattern = /^app-[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}$/;
