20c1912271f5b28eda7bca06413f7392c81a7ffd
Kamioda <kamiodamasato@outlook.com>
2024-09-20 22:42:16 +0900

    ignoreの行数の修正

--- src/AccountManager.ts
+++ src/AccountManager.ts
@@ -22,7 +22,7 @@ export default class AccountManager {
     ) {
         this.CorpProfileGen = new CorpProfileGenerator(NTAAppKey);
     }
-    /* v8 ignore next 8 */
+    /* v8 ignore next 9 */
     [Symbol.asyncDispose]() {
         return Promise.all([
             this.Account[Symbol.asyncDispose](),
