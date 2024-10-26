0d28f61864d956c6daad86e292e49cdcc06e97cb
Kamioda <kamiodamasato@outlook.com>
2024-10-26 20:57:47 +0900

    Authorization Headerを読み取ってパースするメソッドを追加( #6 , #7 )

---
+++ src/Account/TokenPicker.test.ts
@@ -0,0 +1,20 @@
+import PickupToken from "./TokenPicker";
+
+describe('Token Picker Test', () => {
+    it('Basic Authorization', () => {
+        expect(PickupToken('Basic dGVzdDpwYXNzd29yZA==')).toStrictEqual({
+            token_type: 'Basic',
+            id: 'test',
+            password: 'password'
+        });
+    });
+    it('Bearer Authorization', () => {
+        expect(PickupToken('Bearer token')).toStrictEqual({
+            token_type: 'Bearer',
+            token: 'token'
+        });
+    });
+    it('Invalid Authorization', () => {
+        expect(() => PickupToken('Invalid token')).toThrow('Invalid Authorization Header');
+    });
+});
---
+++ src/Account/TokenPicker.ts
@@ -0,0 +1,12 @@
+export type Bearer = { token_type: 'Bearer', token: string };
+export type Basic = { token_type: 'Basic', id: string, password: string };
+
+export default function PickupToken(AuthorizationHeader: string): Bearer | Basic {
+    const [token_type, token] = AuthorizationHeader.split(' ');
+    if (token_type === 'Bearer') return { token_type, token };
+    else if (token_type === 'Basic') {
+        const [id, password] = Buffer.from(token, 'base64').toString().split(':');
+        return { token_type, id, password };
+    }
+    throw new Error('Invalid Authorization Header');
+}
